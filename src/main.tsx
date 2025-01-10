03dd6b5ce6427a1b85512ea90d77eaa0a754e685
Izz <x3zoabdullah@gmail.com>
2025-01-10 17:40:42 +0400

    Added games for PC platform

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
- 
-import HomePage from './Pages/HomePage/HomePage'
 import Header from './Header/Header'
+import HomePage from './Pages/HomePage/HomePage'
 import LeftColumn from './Pages/Quick Navigation/LeftColumn/LeftColumn'
 import Pop2024 from './Pages/Quick Navigation/Popular in 2024/Pop2024'
 import AllTime from './Pages/Quick Navigation/All time top/AllTime'
 import BestOfTheYear from './Pages/Quick Navigation/BestOfTheYear/BestOfTheYear'
-import PlayStation from './Pages/Quick Navigation/Playstation/Playstation'
+import PC from './Pages/Quick Navigation/PC/PC'
 import PlayDice from './Pages/Quick Navigation/PlayDice/PlayDice'
  
  
@@ -20,7 +19,7 @@ createRoot(document.getElementById('root')!).render(
   //<StrictMode>
   <> 
   <Header></Header>
-  <PlayStation></PlayStation>
+  <PC></PC>
   </>
     
  // </StrictMode>,
