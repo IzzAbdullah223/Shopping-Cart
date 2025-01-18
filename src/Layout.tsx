f53ec44365ad4611f1f524037652e9a9f7ca0f60
Izz <x3zoabdullah@gmail.com>
2025-01-19 00:56:29 +0400

    .

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,13 @@
 import {Outlet} from 'react-router-dom'
 import Header from './Header/Header'
+import LayoutCSS from './Layout.module.css'
 
 function Layout(){
     return(
-        <>
+        <div className={LayoutCSS.PageContainer}>
             <Header></Header>
             <Outlet></Outlet>
-        </>
+        </div>
     )
 }
 
